94aa36e0ea1b106718940631c059ed6b91479c31
Muadz Izh <muadzihharul@gmail.com>
2024-07-11 11:32:39 +0300

    Updated

---
+++ public/data.js
@@ -0,0 +1,91 @@
+export const tagStyles = {
+    'Laravel': {
+        color: 'bg-gray-500',
+        icon: 'public/images/tech/laravel.webp', 
+    },
+    'Livewire': {
+        color: 'bg-blue-700',
+        icon: 'public/images/tech/livewire.webp', 
+    },
+    'Tailwind': {
+        color: 'bg-green-700',
+        icon: 'public/images/tech/tailwind.webp', 
+    },
+    'C#': {
+        color: 'bg-purple-700',
+        icon: 'public/images/tech/csharp.webp', 
+    },
+    'WPF': {
+        color: 'bg-yellow-700',
+        icon: 'public/images/tech/wpf.webp', 
+    },
+    'Microsoft SQL': {
+        color: 'bg-slate-800',
+        icon: 'public/images/tech/msql.webp', 
+    },
+};
+
+export const project = [
+    {
+        "name" : "PPI Bandirma - Organisation system",
+        "description" : "Website organisasi untuk mengelola database anggota dan beberapa fitur lainya untuk mendukung kinerja organisasi.",
+        "tags" : "Laravel, Livewire, Tailwind",
+        "image" : "public/images/ppibandirma.png"
+    },
+    {
+        "name" : "Voting system",
+        "description" : "Website untuk mengumpulkan hasil voting.",
+        "tags" : "Laravel, Livewire, Tailwind",
+        "image" : "public/images/musta.png",
+        "github" : ""
+    },
+    {
+        "name" : "Store management",
+        "description" : "Aplikasi windows untuk mengelola produk yang ada di toko. Terdapat fitur kasir dan juga laporan keuangan bulanan",
+        "tags" : "C#, WPF, Microsoft SQL",
+        "image" : "public/images/store-management.png",
+        "github" : "https://github.com/Maviism/storeManagement",
+    },
+    {
+        "name" : "Donasie",
+        "description" : "",
+        "tags" : "Svelte, Payment Gateway",
+        "image": "public/images/donasie.png",
+        "link" : "https://donasie.netlify.app/",
+        "github" : ""
+    },
+    {
+        "name" : "Grafik pelajar indonesia di Turki",
+        "description" : "",
+        "tags" : "JavaScript, D3",
+        "image" : "public/images/grafik-mahasiswa.png",
+        "link" : "https://maviism.github.io/grafik-indonesia-turki"
+    },
+    {
+        "name" : "Associate Data Engineer – Datacamp",
+        "description" : "Certificate",
+        "tags" : "Python",
+        "image" : "",
+    },
+    {
+        "name" : "Flappy Bird",
+        "description" : "",
+        "tags" : "Godot",
+        "image" : "public/images/flappy_bird.png",
+        "link" : "https://maviism.github.io/flappy-bird"
+    },
+    {
+        "name" : "Score Board",
+        "description" : "",
+        "tags" : "Arduino",
+        "image" : "public/images/score_board.png",
+        "link" : "https://www.tinkercad.com/things/iMH2kyJcNYo-score-board"
+    },
+    {
+        "name" : "Responsive web design - Freecodecamp",
+        "description" : "",
+        "tags" : "Html, CSS"
+    }
+    
+
+];
