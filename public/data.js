2ddbeb61ec47a964c8cbde58a52db700f18e4b2f
Muadz Izh <muadzihharul@gmail.com>
2024-07-11 15:32:48 +0300

    updated

--- public/data.js
+++ public/data.js
@@ -1,6 +1,6 @@
 export const tagStyles = {
     'Laravel': {
-        color: 'bg-gray-500',
+        color: 'bg-gray-700',
         icon: 'public/images/tech/laravel.webp', 
     },
     'Livewire': {
@@ -23,12 +23,45 @@ export const tagStyles = {
         color: 'bg-slate-800',
         icon: 'public/images/tech/msql.webp', 
     },
+    'Svelte' : {
+        color: 'bg-orange-700',
+        icon: 'public/images/tech/svelte.webp'
+    },
+    'JavaScript' : {
+        color: 'bg-teal-700',
+        icon: 'public/images/tech/js.webp'
+    },
+    'Python' : {
+        color: 'bg-sky-700',
+        icon: 'public/images/tech/python.webp'
+    },
+    'HTML' : {
+        color: 'bg-violet-700',
+        icon: 'public/images/tech/html.webp'
+    },
+    'CSS' : {
+        color: 'bg-fuchsia-700',
+        icon: 'public/images/tech/css.webp'
+    },
+    'Godot' : {
+        color: 'bg-cyan-700',
+        icon: 'public/images/tech/godot.webp'
+    },
+    'Arduino' : {
+        color: 'bg-indigo-700',
+        icon: 'public/images/tech/arduino.webp'
+    },
+    'D3' : {
+        color: 'bg-rose-700',
+        icon: 'public/images/tech/d3.webp'
+    }
+
 };
 
 export const project = [
     {
-        "name" : "PPI Bandirma - Organisation system",
-        "description" : "Website organisasi untuk mengelola database anggota dan beberapa fitur lainya untuk mendukung kinerja organisasi.",
+        "name" : "PPI Bandirma",
+        "description" : "An organizational website for managing the member database and several other features to support organizational operations.",
         "tags" : "Laravel, Livewire, Tailwind",
         "image" : "public/images/ppibandirma.png"
     },
@@ -41,51 +74,53 @@ export const project = [
     },
     {
         "name" : "Store management",
-        "description" : "Aplikasi windows untuk mengelola produk yang ada di toko. Terdapat fitur kasir dan juga laporan keuangan bulanan",
+        "description" : "A Windows application for managing store products. It features a cashier system, product scanning, and monthly financial reports.",
         "tags" : "C#, WPF, Microsoft SQL",
         "image" : "public/images/store-management.png",
         "github" : "https://github.com/Maviism/storeManagement",
     },
     {
         "name" : "Donasie",
-        "description" : "",
-        "tags" : "Svelte, Payment Gateway",
+        "description" : "In the Donasie project, I delved into using Svelte and integrating payment gateways.",
+        "tags" : "Svelte",
         "image": "public/images/donasie.png",
         "link" : "https://donasie.netlify.app/",
         "github" : ""
     },
     {
-        "name" : "Grafik pelajar indonesia di Turki",
-        "description" : "",
-        "tags" : "JavaScript, D3",
-        "image" : "public/images/grafik-mahasiswa.png",
-        "link" : "https://maviism.github.io/grafik-indonesia-turki"
+        "name": "Grafik Pelajar Indonesia di Turki",
+        "description": "An interactive chart visualizing the number of Indonesian students in Turkey using JavaScript and D3.",
+        "tags": "JavaScript, D3",
+        "image": "public/images/grafik-mahasiswa.png",
+        "link": "https://maviism.github.io/grafik-indonesia-turki"
     },
     {
-        "name" : "Associate Data Engineer – Datacamp",
-        "description" : "Certificate",
-        "tags" : "Python",
-        "image" : "",
+        "name": "[Certificate] Associate Data Engineer – Datacamp",
+        "description": "A certificate demonstrating proficiency in data engineering with Python from Datacamp.",
+        "tags": "Python",
+        "image": "public/images/de-datacamp.png"
     },
     {
-        "name" : "Flappy Bird",
-        "description" : "",
-        "tags" : "Godot",
-        "image" : "public/images/flappy_bird.png",
-        "link" : "https://maviism.github.io/flappy-bird"
+        "name": "Flappy Bird",
+        "description": "A recreation of the classic Flappy Bird game using the Godot engine.",
+        "tags": "Godot",
+        "image": "public/images/flappy_bird.png",
+        "link": "https://maviism.github.io/flappy-bird"
     },
     {
-        "name" : "Score Board",
-        "description" : "",
-        "tags" : "Arduino",
-        "image" : "public/images/score_board.png",
-        "link" : "https://www.tinkercad.com/things/iMH2kyJcNYo-score-board"
+        "name": "Score Board",
+        "description": "An Arduino project for managing and displaying scores in real-time.",
+        "tags": "Arduino",
+        "image": "public/images/score_board.png",
+        "link": "https://www.tinkercad.com/things/iMH2kyJcNYo-score-board"
     },
     {
-        "name" : "Responsive web design - Freecodecamp",
-        "description" : "",
-        "tags" : "Html, CSS"
+        "name": "[Certificate] Responsive Web Design - Freecodecamp",
+        "description": "A certificate for completing the Responsive Web Design course from Freecodecamp.",
+        "tags": "HTML, CSS",
+        "image": "public/images/rwd-freecode.png"
     }
+    
     
 
 ];
